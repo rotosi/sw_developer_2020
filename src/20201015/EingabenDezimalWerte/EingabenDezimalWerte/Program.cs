f7bf62a68596b874863b02a8269cf68cc19b2e1f
Rocio@Wifi <user@WIN10DEV>
2020-10-15 19:52:42 +0200

    Initial solution reading double values within a console application

---
+++ src/20201015/EingabenDezimalWerte/EingabenDezimalWerte/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EingabenDezimalWerte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthA = 0.0;
+            double lengthB = 0.0;
+            double result = 0.0;
+            string input = string.Empty;
+
+            // create header
+            Console.WriteLine("############################################");
+            Console.WriteLine("             Flächen Berechnung");
+            Console.WriteLine("############################################");
+
+            //display unput promt & get length values
+            Console.WriteLine("Bitte Seitenlänge eingeben:\n");
+            Console.WriteLine("\ta: ");
+            input = Console.ReadLine();
+            lengthA = double.Parse(input);
+
+            Console.WriteLine("\tb: ");
+            input = Console.ReadLine();
+            lengthB = int.Parse(input);
+
+            //calculate area of rectangle                                   
+            result = lengthA * lengthB;
+
+            //Display result            
+            Console.WriteLine($"\nFläche des Rechtecks {lengthA} x {lengthB}):  {result}");
+
+
+        }
+    }
+}
