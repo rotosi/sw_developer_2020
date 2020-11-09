37c76d13767f46e4c8113e9aaa8dde1205b54f63
Rocio Torres Sierra <rocio.torressierra@wolfvision.net>
2020-11-09 13:19:37 +0100

    method implementation

--- src/20201103/MethodenGL/MethodenGL/Program.cs
+++ src/20201103/MethodenGL/MethodenGL/Program.cs
@@ -1,170 +1,180 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 
 namespace MethodenGL
 {
-  class Program
-  {
-    static void Main(string[] args)
-
-
+    class Program
     {
-      DisplayHello(); //se llama el metodo creado anteriormente solo con el nombre 
-      DisplayHello();
-      DisplayHello();
-      DisplayHello();
-
-      DisplayColorRedMessage("Hallo liebe Leute!", ConsoleColor.Yellow);
-      DisplayColorRedMessage("Hallo liebe Leute!", ConsoleColor.Red);
-      DisplayColorRedMessage("Hallo liebe Leute!", ConsoleColor.Green);
-
-      double erg = CalculateWeight(20.0); //se declara una variable para del mismo tipo del metodo, se guarda el valor que se ingresa 20.20
-      Console.WriteLine($"ergebnis: {erg}");
-
-
-
-      Console.WriteLine($"Bitte Alter eingeben");
-      string input = Console.ReadLine();
-      int alter = GetInt(input);
-      Console.WriteLine($"Alter: {alter}");
-
-      int eineVariable = GetInt("Bitte länge eingeben");
-      Console.WriteLine("Länger = "+ eineVariable);
-
-      
-
-      
-      double gewicht = GetDouble("Gewicht eingeben");
-      Console.WriteLine($"Das Gewicht ist {gewicht}.");
-
-      DateTime getDate = GetDateTime("Gewicht eingeben");
-      Console.WriteLine($"Das Gewicht ist {DateTime);
-    }
-
-    //signatur
-    //Rückgabetyp Bezeichnung ( [Paramter] ) //in dieser Methode deie erste Bustabe des Name ist immer groß
-    static void DisplayHello()
-    {
-      Console.WriteLine("Hello!");
-    }
-
-    static void DisplayColorRedMessage(string message, ConsoleColor messageColor)
-    {
-      ConsoleColor oldColor = Console.ForegroundColor;
-      Console.ForegroundColor = messageColor;  //se compara si es el mismo color que tiene messageColor // ich mache ein set here (el color que tengo en messegeColor lo asigno a colosone.foregundColor)
-
-      Console.WriteLine(message);
-
-      Console.ForegroundColor = oldColor;
-
-    }
-
-    static double CalculateWeight(double nettoWeight)
-    {
-      double result = 0.0;
-
-      result = nettoWeight * 1.25;
-
-      return result; //lo que se va a retornar tiene que siempre ser una variable del tipo del metodo, en este caso es a double
-    }
-
-
-    ///<sumary>
-    ///</sumary>
-    static int GetInt(string inputPrompt)
-    {
-      int userInputValue = 0;
-      bool userInputIsValid = false;
-
-      do
-      {
-        Console.Write(inputPrompt);
-        try
+        static void Main(string[] args)
         {
-          userInputValue = int.Parse(Console.ReadLine());
-          userInputIsValid = true;
-        }
-        catch (Exception ex)
-        {
-          Console.WriteLine($"Error: {ex.Message}");
-          userInputIsValid = false;
-        }
-      }
-      while (!userInputIsValid);
+            int eineVariable = 12;
 
-      return userInputValue;
+            DisplayHello();
+            DisplayHello();
+            DisplayHello();
+            DisplayHello();
+
+            DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Yellow);
+
+            DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Red);
+            DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Green);
+
+            double erg = CalculateWeight(20.0);//se declara una variable para del mismo tipo del metodo, se guarda el valor que se ingresa 20.20
+            Console.WriteLine($"Ergebnis: {erg}");
+
+            eineVariable = GetInt("Bitte Länge eingeben: ");
+            Console.WriteLine("Länge = " + eineVariable);
+
+            double gewicht = GetDouble("Gewicht eingeben: ");
+            Console.WriteLine($"Das Gewicht ist {gewicht}.");
+
+            DateTime gebDate = GetDateTime("Geburtsdatum eingeben: ");
+            Console.WriteLine($"Sie sind {DateTime.Now.Year - gebDate.Year} Jahre alt.");
+        }
+
+        // Signatur
+        //Rückgabetyp Bezeichnung ([Parameter])//in dieser Methode deie erste Bustabe des Name ist immer groß        
+        static void DisplayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="messageColor"></param>
+        static void DisplayColoredMessage(string message, ConsoleColor messageColor)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = messageColor; ////se compara si es el mismo color que tiene messageColor // ich mache ein set here (el color que tengo en messegeColor lo asigno a colosone.foregundColor)
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = oldColor;
+        }
+
+        // Signatur
+        //Rückgabetyp Bezeichnung ( [Parameter] )        
+        static double CalculateWeight(double nettoWeight)
+        {
+            double result = 0.0;
+
+            result = nettoWeight * 1.25;
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Reads an integer value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns>Input value as integer</returns>
+        static int GetInt(string inputPrompt)
+        {
+            int userInputValue = 0;
+            bool userInputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to int
+                    userInputValue = int.Parse(Console.ReadLine());
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }        
+
+        /// <summary>
+        /// Reads an double value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        static double GetDouble(string inputPrompt)
+        {
+            double userInputValue = 0;
+            bool userInputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to double
+                    userInputValue = double.Parse(Console.ReadLine());
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an DateTime value from console input. The input format must be [dd.MM.yyyy hh:mm:ss].
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        static DateTime GetDateTime(string inputPrompt)
+        {
+            DateTime userInputValue = DateTime.MinValue;
+            bool userInputIsValid = false;
+
+            string inputFormat = "dd.MM.yyyy hh:mm:ss";
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to DateTime
+                    userInputValue = DateTime.ParseExact(Console.ReadLine(), inputFormat, CultureInfo.InvariantCulture);
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an string value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        static string GetString(string inputPrompt)
+        {
+            Console.Write(inputPrompt);
+
+            //read input value and convert to string
+            return Console.ReadLine();
+        }
+
     }
-
-    //==doble; string: DateTime
-
-    static double GetDouble(string inputPrompt)
-    {
-      double userInputValue = 0;
-      bool userInputIsValid = false;
-
-      do
-      {
-        Console.Write(inputPrompt);
-        try
-        {
-          userInputValue = double.Parse(Console.ReadLine());
-          userInputIsValid = true;
-        }
-        catch (Exception ex)
-        {
-          Console.WriteLine($"Error: {ex.Message}");
-          userInputIsValid = false;
-        }
-      }
-      while (!userInputIsValid);
-
-      return userInputValue;
-    }
-
-    static DateTime GetDateTime(string inputPrompt)
-    { 
-    DateTime userInputValue = DateTime.MinValue;
-    bool userInputIsValid = false;
-
-      string inputFormat = "dd.MM.yyyy hh:MM:ss";
-
-      do
-      {
-        Console.Write(inputPrompt);
-        try
-        {
-          userInputValue = DateTime.ParseExact(Console.ReadLine(), inputFormat, cultureInfo.InvariantCulture);
-    userInputIsValid = true;
-        }
-        catch (Exception ex)
-        {
-          Console.WriteLine($"Error: {ex.Message}");
-          userInputIsValid = false;
-        }
-      }
-      while (!userInputIsValid);
-
-      return userInputValue;
-    }
-    /// <summary>
-    /// 
-    /// read an 
-    /// </summary>
-    /// <param name="inputPrompt"></param>
-    /// <returns></returns>
-    static string getString(string inputPrompt)
-    { 
-    string userInputValue = string.Empty;
-    bool userInputIsValid = false;
-     
-     Console.Write(inputPrompt);  
-           
-
-      return Console.ReadLine;
-    }
-
-  }
 }
